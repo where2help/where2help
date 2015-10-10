84cdab7a051875c8c4e00b30a12b2b285b511384
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-10-10 19:53:12 +0200

    authentication method

--- app/controllers/api/v1/needs_controller.rb
+++ app/controllers/api/v1/needs_controller.rb
@@ -2,6 +2,11 @@ module Api
   module V1
     class NeedsController < JSONAPI::ResourceController
     	before_action :authenticate_via_email
+
+    private
+
+    def authenticate_via_email
     end
+    
   end
 end
