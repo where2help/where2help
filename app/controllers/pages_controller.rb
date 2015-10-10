e05f4826654d6ea5acee93baeed221fce9e0a003
Max Mulatz <klappradla@gmail.com>
2015-10-10 19:45:49 +0200

    rake task to seed db with dummy needs, first draft home-feed...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @needs = Need.all
   end
 
   def calendar
--- app/helpers/needs_helper.rb
+++ app/helpers/needs_helper.rb
@@ -1,2 +1,6 @@
 module NeedsHelper
+
+  def icon_for(need)
+    image_tag("needs/#{need.category}.png", class: 'img-responsive')
+  end
 end
