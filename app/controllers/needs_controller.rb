29f209bf0465ceb55a46341d0c4cce41d8a98b26
Max Mulatz <klappradla@gmail.com>
2015-10-11 08:06:08 +0200

    add destroy button to needs edit form

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -51,7 +51,7 @@ class NeedsController < ApplicationController
   def update
     respond_to do |format|
       if @need.update(need_params)
-        format.html { redirect_to @need, notice: 'Need was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Need was successfully updated.' }
         format.json { render :show, status: :ok, location: @need }
       else
         format.html { render :edit }
@@ -65,7 +65,7 @@ class NeedsController < ApplicationController
   def destroy
     @need.destroy
     respond_to do |format|
-      format.html { redirect_to needs_url, notice: 'Need was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Need was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
