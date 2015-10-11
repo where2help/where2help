ffbbc11042e486cd63983e109542be7371caf14d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-10-11 15:19:04 +0200

    create volunteering from api

--- app/controllers/api/v1/volunteerings_controller.rb
+++ app/controllers/api/v1/volunteerings_controller.rb
@@ -1,6 +1,20 @@
 module Api
   module V1
     class VolunteeringsController < JSONAPI::ResourceController
+
+      def create
+        user = User.find_by(email: params['email'])
+
+        unless user
+          user = User.new(email: params['email'])
+          user.save(validate: false)
+        end
+
+        volunteering = Volunteering.create(user_id: user.id, need_id: params['need_id'])
+
+        volunteering_json = JSONAPI::ResourceSerializer.new(VolunteeringResource).serialize_to_hash(VolunteeringResource.new(volunteering, nil))
+        render json: volunteering_json
+      end
     end
   end
 end
--- app/resources/api/v1/session_resource.rb
+++
@@ -1,7 +0,0 @@
-module Api
-  module V1
-    class SessionResource < JSONAPI::Resource
-      abstract
-    end
-  end
-end
