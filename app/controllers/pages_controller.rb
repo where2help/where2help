976ff2f7eafa48ec53541e0fa5467430c58f885a
Max Mulatz <klappradla@gmail.com>
2015-10-11 08:18:07 +0200

    disable need buttons while transaction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    if current_user.admin?
+    if user_signed_in? && current_user.admin?
       redirect_to pages_calendar_path
     else
       redirect_to needs_feed_path
