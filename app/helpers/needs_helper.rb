a3383b92e3eeed3466175cf231f228a11061a6b8
Max Mulatz <klappradla@gmail.com>
2015-10-11 08:09:34 +0200

    loading icon to feed button

--- app/helpers/needs_helper.rb
+++ app/helpers/needs_helper.rb
@@ -7,7 +7,7 @@ module NeedsHelper
   def view_more_link(resource)
     link_to_next_page resource, 'Mehr anzeigen',
       remote: true,
-      data: { disable_with: 'lädt...' },
-      class: 'btn btn-primary view-more'
+      data: { disable_with: "<i class='fa fa-spinner fa-spin'></i>" },
+      class: 'btn btn-primary btn-lg btn-block view-more'
   end
 end
