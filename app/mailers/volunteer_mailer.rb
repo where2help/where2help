a52ea48b14a49b2befcee62d8ccf1d49bfaba993
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2015-10-15 18:36:31 +0200

    Add url for where2help

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -4,6 +4,7 @@ class VolunteerMailer < ApplicationMailer
   def notify_volunteers(need)
     @volunteers = User.where.not(ngo_admin: true)
     @need = need
+    @url = 'http://where2help.at'
     mail(to: @volunteers.map(&:email), subject: "Wir brauchen dir am #{need.location}!")
   end
 
