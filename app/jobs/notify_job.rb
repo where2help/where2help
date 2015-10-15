c65575ee27f7756ad7056422a329ae3ca7e6e652
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2015-10-15 18:42:29 +0200

    Clean up code

--- app/jobs/notify_job.rb
+++ app/jobs/notify_job.rb
@@ -2,11 +2,6 @@ class NotifyJob
   include SuckerPunch::Job
 
   def perform(need)
-    #Log.new(event).track
-    #Rails.logger.debug event
-
-    require 'pry'
-    binding.pry
     VolunteerMailer.notify_volunteers(need).deliver
   end
 end
