b3048603029b783bb88e92d35bfecb634f41447e
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-10-15 15:35:43 +0200

    move namespace

---
+++ app/resources/api/calendar/need_resource.rb
@@ -0,0 +1,11 @@
+module Api
+  module Calendar
+    class NeedResource < JSONAPI::Resource
+      attributes :location, :start_time, :end_time, :volunteers_needed, :city, :category, :user_id, :volunteers_count
+
+      def volunteers_count
+        @model.volunteers.count
+      end
+    end
+  end
+end
