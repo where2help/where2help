03c5a49277c2dcc832bfc1d01530e315a6ec88a9
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2015-10-15 18:27:17 +0200

    Implement mailer mail action

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,2 +1,10 @@
 class VolunteerMailer < ApplicationMailer
+  default from: 'admin@where2help.at'
+
+  def notify_volunteers(need)
+    @volunteers = User.where.not(ngo_admin: true)
+    @need = need
+    mail(to: @volunteers.map(&:email), subject: "Wir brauchen dir am #{need.location}!")
+  end
+
 end
