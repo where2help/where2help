5abfc9e58c7aca5af28a758afe24d5edc17af932
Max Mulatz <klappradla@gmail.com>
2015-10-16 11:38:41 +0200

    add redirects for root path when logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  def home    
+  def home
+    redirect_to after_sign_in_path_for(current_user) if user_signed_in?
   end
   
   def calendar
