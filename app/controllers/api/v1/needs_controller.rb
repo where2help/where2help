03bc05441c51b392dd779c55f19e704e1e747112
Aaron Cruz <aaron@aaroncruz.com>
2015-10-19 16:05:13 +0200

    Clean up needs create action

--- app/controllers/api/v1/needs_controller.rb
+++ app/controllers/api/v1/needs_controller.rb
@@ -5,15 +5,11 @@ module Api
       before_filter :authenticate_user!
 
       def create
-        sparams = params['data']['attributes']
+        sparams    = params['data']['attributes']
         start_time = sparams['start-time']
-        end_time = sparams['end-time']
-        user_id = current_user.id
-
-        need = Need.create!(start_time: start_time, end_time: end_time, user_id: user_id)
-        need.save
-
-        need_json = JSONAPI::ResourceSerializer.new(NeedResource).serialize_to_hash(NeedResource.new(need, nil))
+        end_time   = sparams['end-time']
+        need       = current_user.needs.create!(start_time: start_time, end_time: end_time)
+        need_json  = JSONAPI::ResourceSerializer.new(NeedResource).serialize_to_hash(NeedResource.new(need, nil))
         render json: need_json
       end
 
