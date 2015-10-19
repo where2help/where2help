21de49a9f15f41e4fad95b0804add7e99dc701ab
Aaron Cruz <aaron@aaroncruz.com>
2015-10-19 16:19:14 +0200

    Make lat, lng work on need update

--- app/workers/workers/coords.rb
+++ app/workers/workers/coords.rb
@@ -1,10 +1,12 @@
 require 'jobs/coords'
 
-class Workers::Coords
-  include SuckerPunch::Job
+module Workers
+  class Coords
+    include SuckerPunch::Job
 
-  def perform(need_id)
-    need = Need.find(need_id)
-    Jobs::Coords.add_to(need)
+    def perform(need_id)
+      need = Need.find(need_id)
+      Jobs::Coords.add_to(need)
+    end
   end
 end
--- lib/jobs/coords.rb
+++ lib/jobs/coords.rb
@@ -1,18 +1,22 @@
 require 'clients'
-class Jobs::Coords
-  def add_to(need)
-    client  = Maps.new
-    address = location(need)
-    coords  = client.coords(address: address)
-    update_need!(need) if coords
-  end
+module Jobs
+  class Coords
+    class << self
+      def add_to(need)
+        client  = Maps.new
+        address = location(need)
+        coords  = client.coords(address: address)
+        update_need!(need, coords) if coords
+      end
 
-  def update_need!(need)
-    need.update_attributes(lat: coords.lat, lng: coords.lng)
-  end
+      def update_need!(need, coords)
+        need.update_attributes(lat: coords.lat, lng: coords.lng)
+      end
 
-  def location(need)
-    city, place = need.city, need.location
-    return "%s, %s" % [city, place]
+      def location(need)
+        city, place = need.city, need.location
+        return "%s, %s" % [city, place]
+      end
+    end
   end
 end
