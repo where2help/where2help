ee632f88f26b6d80bbea754848be7ec7f5261810
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-10-28 21:06:33 +0100

    accept locale param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,15 @@ class ApplicationController < ActionController::Base
 
   # params for sign_up
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   before_action :authenticate_user!
+  before_action :set_locale
 
   protected
+
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
