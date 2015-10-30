bf84ba720b93757522d531c21c253b28239b863d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-10-30 16:09:36 +0100

    volunteering api back to standard

--- app/controllers/api/v1/volunteerings_controller.rb
+++ app/controllers/api/v1/volunteerings_controller.rb
@@ -1,21 +1,6 @@
 module Api
   module V1
     class VolunteeringsController < ApiController
-
-      def create
-      	sparams = params['data']['attributes']
-        user = User.find_by(email: params['email'])
-
-        unless user
-          user = User.new(email: params['email'])
-          user.save(validate: false)
-        end
-
-        volunteering = Volunteering.create(user_id: user.id, need_id: sparams['need_id'])
-
-        volunteering_json = JSONAPI::ResourceSerializer.new(VolunteeringResource).serialize_to_hash(VolunteeringResource.new(volunteering, nil))
-        render json: volunteering_json
-      end
     end
   end
 end
