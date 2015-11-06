0f3dfabc5f019f434d7ca146e4cf60a7f713c33a
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-11-06 19:23:04 +0100

    prevent from subscribing to volunteering twice

--- app/models/volunteering.rb
+++ app/models/volunteering.rb
@@ -1,4 +1,9 @@
 class Volunteering < ActiveRecord::Base
   belongs_to :user
   belongs_to :need, counter_cache: true
+
+   validates :user_id, uniqueness: {
+     scope: :need_id,
+     message: "Sie sind bereits als Helfer eingetragen."
+   }
 end
