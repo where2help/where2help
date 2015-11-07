6712eab5b40c497c87602422f21347f5dbcde4d1
Max Mulatz <klappradla@gmail.com>
2015-11-07 17:03:35 +0100

    registration form volunteer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'norply@where2help.org'
+  config.mailer_sender = 'no-reply@where2help.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
