bf69633eff9a6fb307e38d7d3ebebe6d1850fa9d
Max Mulatz <klappradla@gmail.com>
2015-11-09 23:23:14 +0100

    user appointments paths

--- app/controllers/volunteers/needs_controller.rb
+++ app/controllers/volunteers/needs_controller.rb
@@ -1,13 +1,9 @@
 class Volunteers::NeedsController < ApplicationController
-
+  
   def index
     @needs = current_user.appointments.
                           filter_scope(params[:scope] || 'upcoming').
                           filter_place(params[:place]).
                           page(params[:page]).per(10)
-  end
-
-  def show
-    @need = Need.find(params[:id])
   end
 end
