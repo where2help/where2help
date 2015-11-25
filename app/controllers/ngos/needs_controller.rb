c29f82e1a60febb1a3156c9b206f24877ad08158
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2015-11-25 19:49:46 +0100

    Fix bug where needs couldn't be updated from the Calendar

--- app/controllers/ngos/needs_controller.rb
+++ app/controllers/ngos/needs_controller.rb
@@ -3,4 +3,21 @@ class Ngos::NeedsController < NeedsController
 
   def calendar
   end
+
+  # PATCH/PUT /needs/1
+  # PATCH/PUT /needs/1.json
+  def update
+    respond_to do |format|
+      if @need.update(need_params)
+        # Add lat and lng to need
+        Workers::Coords.new.async.perform(@need.id)
+        format.html { redirect_to @need, notice: 'Need was successfully updated.' }
+        format.json { render :show, status: :ok, location: @need }
+      else
+        format.html { render :edit }
+        format.json { render json: @need.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
