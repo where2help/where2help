bc1eb3d30b339934f5b78396982c514618e25ac8
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-11-28 11:42:17 +0100

    needs should not be modified via the api

--- app/controllers/api/v1/needs_controller.rb
+++ app/controllers/api/v1/needs_controller.rb
@@ -2,21 +2,6 @@ module Api
   module V1
     class NeedsController < ApiController
       skip_before_filter :authenticate_user!, only: [:index, :show]
-
-      def create
-        sparams    = params['data']['attributes']
-        start_time = sparams['start-time']
-        end_time   = sparams['end-time']
-        need       = current_user.needs.create!(start_time: start_time, end_time: end_time)
-        need_json  = JSONAPI::ResourceSerializer.new(NeedResource).serialize_to_hash(NeedResource.new(need, nil))
-        render json: need_json
-      end
-
-      def update
-        super
-        ::NotifyJob.new.perform("notify!!!!!!!!!!!")
-        Rails.logger.debug '=========================lslslalfas'
-      end
     end
   end
 end
