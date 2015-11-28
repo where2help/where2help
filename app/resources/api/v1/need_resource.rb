23cfb7d4af9c8020b93fe15972144fe625285b5a
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-11-28 10:14:51 +0100

    update jsonapi-resources, needs controller is now immutable

--- app/resources/api/v1/need_resource.rb
+++ app/resources/api/v1/need_resource.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class NeedResource < JSONAPI::Resource
+      immutable
+
       attributes :location, :start_time, :end_time, :volunteers_needed,
                  :city, :category, :user_id, :volunteers_count,
                  :lat, :lng, :description
