70856aa9c9268d28ef5c9f0ba298cafbdddc996d
Aaron Cruz <aaron@aaroncruz.com>
2015-12-11 11:17:23 +0100

    Coords job falls back to city

--- lib/jobs/coords.rb
+++ lib/jobs/coords.rb
@@ -1,13 +1,20 @@
 require 'clients'
 module Jobs
   class Coords
+    COUNTRY="Austria"
     class << self
       def add_to(need)
         client  = Maps.new
         address = location(need)
         coords  = client.coords(address: address)
+        if invalid?(coords)
+          address = city_only(need)
+          coords  = client.coords(address: address)
+        end
         update_need!(need, coords) if coords
       end
+
+      private
 
       def update_need!(need, coords)
         need.update_attributes(lat: coords.lat, lng: coords.lng)
@@ -16,6 +23,15 @@ module Jobs
       def location(need)
         city, place = need.city, need.location
         return "%s, %s" % [place, city]
+      end
+
+      def city_only(need)
+        city = need.city
+        return "%s, %s" % [city, COUNTRY]
+      end
+
+      def invalid?(coords)
+        coords.nil? || coords.lat.nil? || coords.lng.nil?
       end
     end
   end
