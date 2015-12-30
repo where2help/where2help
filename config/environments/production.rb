baabdf1e0962294659e12e4ab9c0c4f4512fb642
Max Mulatz <klappradla@gmail.com>
2015-12-30 12:14:43 +0100

    add mailer config for production env close #69

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,23 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = {
+    from: 'where2help <no-reply@where2help.herokuapp.com>',
+    reply_to: 'where2help <where2helpinaustria@gmail.com>'
+  }
+  config.action_mailer.default_url_options = { host: 'where2help.herokuapp.com' }
+  config.action_mailer.default charset: 'utf-8'
+
+  # Sendgrid Settings
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -81,15 +98,4 @@ Rails.application.configure do
 
   # Devise needs that on Heroku (Stefan Haslinger)
   config.assets.initialize_on_precompile = false
-
-  # Sendgrid Settings
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 end
