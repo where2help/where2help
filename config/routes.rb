8dd2874f00041c62c04bca3ed26eee36e16361ac
ramon huidobro <ramon.martin.huidobro@gmail.com>
2016-02-18 16:08:42 +0100

    Generate views and controllers for ngos and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :ngos
+  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :ngos, controllers: { sessions: "ngos/sessions" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
