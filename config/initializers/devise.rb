f398a37bfabad19535f6d81904bdc1177b47f7cd
ramon huidobro <ramon.martin.huidobro@gmail.com>
2016-02-18 14:59:33 +0100

    Configure to use diff. views for ngos and users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,8 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.scoped_views = true
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
