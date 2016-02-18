2ad39f517af0327a7415701b100092c5dce34730
ramon huidobro <ramon.martin.huidobro@gmail.com>
2016-02-18 16:40:53 +0100

    Add language model with specs

---
+++ app/models/language.rb
@@ -0,0 +1,4 @@
+class Language < ApplicationRecord
+  validates_presence_of :name
+  validates_uniqueness_of :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218151143) do
+ActiveRecord::Schema.define(version: 20160218153152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ngos", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
