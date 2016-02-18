58cbfe86ae3f2600548a4e7d6748e68776d52845
ramon huidobro <ramon.martin.huidobro@gmail.com>
2016-02-18 15:41:01 +0100

    Tweak migrations and setups of each devise model

--- db/migrate/20160218134703_devise_create_ngos.rb
+++ db/migrate/20160218134703_devise_create_ngos.rb
@@ -13,30 +13,34 @@ class DeviseCreateNgos < ActiveRecord::Migration
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
+      #t.integer  :sign_in_count, default: 0, null: false
+      #t.datetime :current_sign_in_at
+      #t.datetime :last_sign_in_at
+      #t.inet     :current_sign_in_ip
+      #t.inet     :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      #t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name
+      t.string :identifier
+      t.string :locale
+      t.datetime :admin_confirmed_at
       t.timestamps null: false
+
     end
 
     add_index :ngos, :email,                unique: true
     add_index :ngos, :reset_password_token, unique: true
-    # add_index :ngos, :confirmation_token,   unique: true
+    add_index :ngos, :confirmation_token,   unique: true
     # add_index :ngos, :unlock_token,         unique: true
   end
 end
