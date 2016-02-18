17c60718d5d1ec08f8e5a6a654870044580e4dbe
ramon huidobro <ramon.martin.huidobro@gmail.com>
2016-02-18 16:55:59 +0100

    Add factory validity spec to language model

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
+  it 'has a valid factory' do
+    expect(create :language).to be_valid
+  end
   it { is_expected.to validate_presence_of :name}
   it { is_expected.to validate_uniqueness_of :name}
 end
