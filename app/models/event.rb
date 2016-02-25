4008cf8ae636dc65d03bb4bc0599b133f122e1f0
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-02-25 17:52:55 +0100

    Make shifts destroy dependent on events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :shifts
+  has_many :shifts, dependent: :destroy
 
   validates :category, :volunteers_needed, :starts_at, :ends_at, presence: true
   validates :shift_length, :address, presence: true
