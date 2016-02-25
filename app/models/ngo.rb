e558a26faa9ea61c97420e6f6b50166ac7535659
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-02-25 16:05:20 +0100

    Define locale enum for NGO

--- app/models/ngo.rb
+++ app/models/ngo.rb
@@ -2,6 +2,8 @@ class Ngo < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :validatable
 
+  enum locale: { de: 0, en: 1 }
+
   has_one :contact, dependent: :destroy
 
   validates :name, presence: true
