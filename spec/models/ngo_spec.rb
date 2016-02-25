e1ac914b575efd39891a8c42f767c466a063d3e9
Max Mulatz <klappradla@gmail.com>
2016-02-25 17:56:17 +0100

    minify ngo model specs like yeah

--- spec/models/ngo_spec.rb
+++ spec/models/ngo_spec.rb
@@ -15,18 +15,7 @@ RSpec.describe Ngo, type: :model do
   it { is_expected.to define_enum_for(:locale).with([:de, :en])}
 
   describe 'associations' do
-    let(:ngo) { create :ngo }
-
-    describe 'contact' do
-      it { is_expected.to have_one :contact }
-
-      it 'destroys contact on destroy' do
-        create :contact, ngo: ngo
-        expect{
-          ngo.destroy
-        }.to change{Contact.count}.by -1
-      end
-    end
+    it { is_expected.to have_one(:contact).dependent :destroy }
   end
 
   describe 'callbacks' do
