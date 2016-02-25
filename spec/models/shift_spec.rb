817f97f5ee79841591ab1cbe44b6ef5f38949c5f
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-02-25 17:43:37 +0100

    Add validation and belonging specs to shifts

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Shift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:volunteers_needed)}
+  it { is_expected.to validate_presence_of(:starts_at)}
+  it { is_expected.to validate_presence_of(:ends_at)}
+
+  it { is_expected.to belong_to(:event)}
 end
