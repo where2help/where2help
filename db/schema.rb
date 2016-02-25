f67852236f4a5c1d48661c076a9cd20fd9ca6de5
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-02-25 18:12:46 +0100

    Migrate db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225163054) do
+ActiveRecord::Schema.define(version: 20160225170422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,15 @@ ActiveRecord::Schema.define(version: 20160225163054) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.index ["event_id"], name: "index_shifts_on_event_id", using: :btree
+  end
+
+  create_table "shifts_users", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "shift_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shift_id"], name: "index_shifts_users_on_shift_id", using: :btree
+    t.index ["user_id"], name: "index_shifts_users_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
