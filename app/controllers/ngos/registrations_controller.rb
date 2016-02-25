73b5b7eb4bed74484d6049cb129a0271915066e5
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-02-25 16:40:25 +0100

    Allow locale param when updating NGO

--- app/controllers/ngos/registrations_controller.rb
+++ app/controllers/ngos/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Ngos::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -58,9 +58,15 @@ class Ngos::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update) do |ngo_params|
+      ngo_params.permit(
+        :email,
+        :password, :password_confirmation, :current_password,
+        :locale
+      )
+    end
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
