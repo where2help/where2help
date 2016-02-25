16252f5bda72ad16bb77d206003934bfdeae545d
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-02-25 15:42:00 +0100

    Change db locale to be integer w. default value 0

---
+++ db/migrate/20160225141453_change_locale_to_be_enum.rb
@@ -0,0 +1,10 @@
+class ChangeLocaleToBeEnum < ActiveRecord::Migration[5.0]
+  def up
+    remove_column :users, :locale
+    add_column :users, :locale, :integer, default: 0
+  end
+
+  def down
+    change_column :users, :locale, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219133638) do
+ActiveRecord::Schema.define(version: 20160225141453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,12 +92,12 @@ ActiveRecord::Schema.define(version: 20160219133638) do
     t.datetime "confirmation_sent_at"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "locale"
     t.boolean  "admin",                  default: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "api_token"
     t.datetime "api_token_valid_until"
+    t.integer  "locale",                 default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
