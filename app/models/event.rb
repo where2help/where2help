cae9f84c94fe85ffb144cb065a2ede12b4ffd12f
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-02-25 17:19:21 +0100

    Implement Event field validation and enum for cat.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  validates :category, :volunteers_needed, :starts_at, :ends_at, presence: true
+  validates :shift_length, :address, presence: true
+
+  enum category: { volunteer: 0, medical: 1, legal: 2 }
 end
