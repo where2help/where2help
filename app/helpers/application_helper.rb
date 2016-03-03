254053ecea39dc3ac68f21a3a14e13eb835daf18
Max Mulatz <klappradla@gmail.com>
2016-03-03 17:22:58 +0100

    add separate navigation for all types of visitors (#83)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def navigation_for(visitor)
+    if visitor
+      visitor_type = visitor.class.name.demodulize.underscore.pluralize
+      render "#{visitor_type}/nav"
+    else
+      render 'nav'
+    end
+  end
 end
