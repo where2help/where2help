f9a19ce6f511fb2d50a4c62bfe26fec8fadbb0a9
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-03-10 17:48:18 +0100

    Add a shift to our soon-to-be-created event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,8 @@
-class EventsController < InheritedResources::Base
+class EventsController < ApplicationController
   before_filter :authenticate_ngo!, only: :new
 
+  def new
+    @event = Event.new
+    #@event.shifts.build(volunteers_needed: 1, starts_at: Time.now, ends_at: 2.hours.from_now)
+  end
 end
