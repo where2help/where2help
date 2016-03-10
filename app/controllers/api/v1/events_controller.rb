4c31e1406a6537a6fc067fef130e333d5f371675
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-03-10 15:53:56 +0100

    event api endpoints with minimal output

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,9 +3,15 @@ class Api::V1::EventsController < Api::V1::ApiController
   # wget --header="Authorization: Token token=scWTF92WXNiH2WhsjueJk4dN" -S http://localhost:3000/api/v1/events/
   def index
     @events = Event.where("starts_at > ?", Time.now).where.not(volunteers_needed: 0)
+    if params[:minimal] 
+      render :minimal_index and return
+    end
   end
 
   def show
     @event = Event.find(params[:id])
+    if params[:minimal] 
+      render :minimal_show and return
+    end
   end
 end
---
+++ app/views/api/v1/events/minimal_index.json.jbuilder
@@ -0,0 +1,11 @@
+json.array!(@events) do |event|
+  json.extract! event, :id, 
+                       :volunteers_needed
+
+  json.shifts event.shifts do |shift|  
+    json.extract! shift, :id, 
+                        :volunteers_needed, 
+                        :volunteers_count 
+    json.current_user_assigned shift.users.include?(current_user)
+  end
+end
