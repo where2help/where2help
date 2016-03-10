92541c3789eb25c6584ce3951a19e868517612db
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-03-10 18:05:44 +0100

    Uncomment code

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,6 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new
-    #@event.shifts.build(volunteers_needed: 1, starts_at: Time.now, ends_at: 2.hours.from_now)
+    @event.shifts.build(volunteers_needed: 1, starts_at: Time.now, ends_at: 2.hours.from_now)
   end
 end
