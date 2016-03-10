f828b3212891127c340df0e9a5ec34636f49474c
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-03-10 18:16:48 +0100

    Implement event create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,24 @@ class EventsController < ApplicationController
     @event = Event.new
     @event.shifts.build(volunteers_needed: 1, starts_at: Time.now, ends_at: 2.hours.from_now)
   end
+
+  def create
+    @event = Event.new(event_params)
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+      else
+        format.html { render action: :new }
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :description, :address, :shift_length,
+      shifts_attributes: [:id, :volunteers_needed, :starts_at, :ends_at]
+    )
+  end
 end
