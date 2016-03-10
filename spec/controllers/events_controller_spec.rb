c8e62c47ce2f28fae75fda8117cdd9c157aada9d
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-03-10 16:26:40 +0100

    Add specs for authentication

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,11 +2,32 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
+  before do
+    @request.env['devise.mapping'] = Devise.mappings[:ngo]
   end
 
+  describe "GET #new" do
+    context 'given a signed in NGO' do
+      before do
+        sign_in :ngo, FactoryGirl.create(:confirmed_ngo)
+      end
+
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'given a signed in user' do
+      before do
+        sign_in :user, FactoryGirl.create(:user)
+      end
+
+      it "redirects to the NGO sign in page" do
+        get :new
+        expect(response).to redirect_to(new_ngo_session_path)
+      end
+    end
+
+  end
 end
--- spec/factories/ngos.rb
+++ spec/factories/ngos.rb
@@ -5,5 +5,9 @@ FactoryGirl.define do
     identifier { Faker::Company.ein }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+    factory :confirmed_ngo do
+      confirmed_at Time.now
+      aasm_state 'admin_confirmed'
+    end
   end
 end
