44615da8537ef8cffb230a8aa9b6ab54811f2222
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-03-17 14:43:54 +0100

    changed shifts derivation

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,4 +3,6 @@ class Shift < ApplicationRecord
   has_many :users, through: :shifts_users
   belongs_to :event
   validates :volunteers_needed, :starts_at, :ends_at, presence: true
+
+  scope :is_not_full, -> { where("volunteers_needed <= volunteers_count") } 
 end
