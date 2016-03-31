e8db8f7725b48b1a1959ff74d6b8f82849e52af5
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-03-31 15:21:12 +0200

    Touch up event controller specs and include #index

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,10 +1,7 @@
 FactoryGirl.define do
   factory :event do
-    title { Faker::StarWars.quote }
+    title { Faker::StarWars.planet }
     description { Faker::Hipster.paragraph(2) }
-    volunteers_needed 1
-    starts_at "2016-02-25 17:01:35"
-    ends_at "2016-02-25 17:01:35"
     shift_length 1
     address "MyString"
     lat 1.5
