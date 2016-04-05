90a22388cdbada3c8ea7c52674c7ea624f15c25d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-04-05 19:32:43 +0200

    bracket missing

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -4,6 +4,6 @@ class Shift < ApplicationRecord
   belongs_to :event
   validates :volunteers_needed, :starts_at, :ends_at, presence: true
 
-  default_scope { order(starts_at: :asc }
+  default_scope { order(starts_at: :asc) }
   scope :is_not_full, -> { where("volunteers_needed > volunteers_count") }
 end
