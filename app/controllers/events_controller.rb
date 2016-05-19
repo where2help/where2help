6132b93b01b46e789d911474a565415f353e45d5
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-05-19 16:12:23 +0200

    try to fix duplicate entries

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,7 @@ class EventsController < ApplicationController
       where('shifts.volunteers_needed > shifts.volunteers_count').
       where('shifts.starts_at > NOW()').
       order('shifts.starts_at ASC').
+      uniq.
       page(params[:page])
 
     if params[:last_date] && params[:last_date].to_date == @events.first.starts_at.to_date
