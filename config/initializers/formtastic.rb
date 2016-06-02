9750b0f3c7f7138a098e9ef45ac53e5091e0e77e
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-06-02 16:52:17 +0200

    placeholder texts for ngo event form, fixes #134

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -52,11 +52,11 @@
 # Specifies if labels/hints for input fields automatically be looked up using I18n.
 # Default value: true. Overridden for specific fields by setting value to true,
 # i.e. :label => true, or :hint => true (or opposite depending on initialized value)
-# Formtastic::FormBuilder.i18n_lookups_by_default = false
+Formtastic::FormBuilder.i18n_lookups_by_default = true
 
 # Specifies if I18n lookups of the default I18n Localizer should be cached to improve performance.
 # Defaults to true.
-# Formtastic::FormBuilder.i18n_cache_lookups = false
+Formtastic::FormBuilder.i18n_cache_lookups = false
 
 # Specifies the class to use for localization lookups. You can create your own
 # class and use it instead by subclassing Formtastic::Localizer (which is the default).
