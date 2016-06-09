7d076e828ca3809f2947c84bc2b2c827c3d72da2
Max Mulatz <klappradla@gmail.com>
2016-06-09 14:14:06 +0200

    tmp solution for visible and invisible events (ref: #141)

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -12,16 +12,27 @@ module EventsHelper
   end
 
   def format_description(description)
-    simple_format(auto_link description)
+    if description.blank?
+      '-'
+    else
+      simple_format(auto_link description)
+    end
   end
 
   def label_for_event_state(event)
+    if event.pending?
+      state_class = 'label'
+      icon_class = 'fa-eye-slash'
+    else
+      state_class = 'label label-info'
+      icon_class = 'fa-eye'
+    end
     state = t "activerecord.attributes.event.state/" + event.aasm.current_state.to_s
-    state_class = event.pending? ? 'label' : 'label label-success'
+    concat content_tag(:i, nil, class: "fa-li fa #{icon_class} fa-fw")
     content_tag(:span, state, class: state_class)
   end
 
   def time_for_event(event)
-    "#{event.shifts.first.starts_at.to_s(:time)} - #{event.shifts.last.ends_at.to_s(:time)}"    
+    "#{event.shifts.first.starts_at.to_s(:time)} - #{event.shifts.last.ends_at.to_s(:time)}"
   end
 end
