ad1991d74a1831d6463762e7e576476fad58e36a
Max Mulatz <klappradla@gmail.com>
2016-06-30 15:04:55 +0200

    formatting :v:

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,9 +30,9 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   [:controller, :view, :request].each do |type|
-    config.include ::Rails::Controller::Testing::TestProcess, :type => type
-    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
-    config.include ::Rails::Controller::Testing::Integration, :type => type
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
   end
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
