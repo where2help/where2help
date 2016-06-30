4307740a3b171623ff91ec28c77463fffc5fe6d5
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-06-30 16:56:28 +0200

    api/registrations specs

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Registrations", :type => :request do
-  describe "GET /authors/:id" do
+  describe "POST /users/register" do
     it 'creates a new user for proper attributes' do
       user = { first_name: "Jane", last_name: "Doe", email: "jane@doe.com",
                phone: "1234567890", password: "supersecret", password_confirmation: "supersecret" }
                
-      post "/api/v1/users/register", user, { "ACCEPT" => "application/json" }
+      post "/api/v1/users/register", params: user, as: :json
       
       expect(response).to be_success
       expect(json).to include_json user.except(:password, :password_confirmation)
+    end
+
+    it 'fails to create user if email missing' do
+        user = { first_name: "Jane", last_name: "Doe", phone: "1234567890" }
+                 
+        post "/api/v1/users/register", params: user, as: :json
+       
+        expect(response).to have_http_status(400)
+        expect(json).to include_json({ email: ["muss ausgefüllt werden"], 
+                                       password: ["muss ausgefüllt werden"] })
     end
   end
 end
