c008555e8e978185c7b3dd9c70bf1ad3891f7a3d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-07-07 17:33:31 +0200

    handles non existing shift properly, when trying to opt out

--- spec/requests/api/v1/shifts_spec.rb
+++ spec/requests/api/v1/shifts_spec.rb
@@ -29,5 +29,13 @@ RSpec.describe "Shifts", :type => :request do
       expect(json).to include_json({opted_out: true})
       expect(Participation.find_by(shift_id: shift.id, user_id: User.first.id)).to be nil
     end
+
+    it 'captures the situation of a non exsting shift' do
+      post "/api/v1/shifts/opt_out", params: { shift_id: 42 }, 
+                                     headers: token_header,
+                                     as: :json
+      expect(response).to be_not_found
+      expect(json).to include_json({opted_out: false})
+    end
   end
 end
