615c6c7c7b0fd995a7cf45fed03da05cc427e9d9
Stefan Haslinger <stefan.haslinger@informatom.com>
2016-07-14 17:38:35 +0200

    api events controller tests done!

--- spec/requests/api/v1/events_spec.rb
+++ spec/requests/api/v1/events_spec.rb
@@ -29,4 +29,70 @@ RSpec.describe "Events", type: :request do
                                     }])
     end
   end  
+
+  describe "GET /events?minimal=true" do
+    it 'returns the events with minimal json' do
+      ngo = create :ngo
+      event = create :event, :with_shift, state: :published,
+                                          ngo: ngo
+      get "/api/v1/events?minimal=true", as: :json, headers: token_header
+      expect(response).to be_success
+      expect(json).to include_json([{"id" => event.id,
+                                     "shifts" =>
+                                     [{"id" => event.shifts.first.id,
+                                       "volunteers_needed" => event.shifts.first.volunteers_needed,
+                                       "volunteers_count" => event.shifts.first.volunteers_count,
+                                       "current_user_assigned" => false
+                                    }] 
+                                 }])
+    end
+  end  
+
+
+  describe "GET /events/:id" do
+    it 'returns the event' do
+      ngo = create :ngo
+      event = create :event, :with_shift, state: :published,
+                                          ngo: ngo
+      get "/api/v1/events/" + event.id.to_s, as: :json, headers: token_header
+
+      expect(response).to be_success
+      expect(json).to include_json({"id" => event.id,
+                                    "title" => event.title,
+                                    "description" => event.description,
+                                    "address" => event.address,
+                                    "lat" => event.lat,
+                                    "lng" => event.lng,
+                                    "state" => event.state,
+                                    "organization_name" => event.ngo.name,
+                                    "shifts" =>
+                                    [{"id" => event.shifts.first.id,
+                                      "event_id" => event.id,
+                                      "starts_at"=> event.shifts.first.starts_at.strftime("%FT%T.%L%:z"),
+                                      "ends_at" => event.shifts.first.ends_at.strftime("%FT%T.%L%:z"),
+                                      "volunteers_needed" => event.shifts.first.volunteers_needed,
+                                      "volunteers_count" => event.shifts.first.volunteers_count,
+                                      "current_user_assigned" => false
+                                    }] 
+                                  })
+    end
+  end  
+  
+  describe "GET /events/:id?minimal=true" do
+    it 'returns the event with minimal json' do
+      ngo = create :ngo
+      event = create :event, :with_shift, state: :published,
+                                          ngo: ngo
+      get "/api/v1/events/" + event.id.to_s + "?minimal=true", as: :json, headers: token_header
+      expect(response).to be_success
+      expect(json).to include_json({"id" => event.id,
+                                    "shifts" =>
+                                    [{"id" => event.shifts.first.id,
+                                      "volunteers_needed" => event.shifts.first.volunteers_needed,
+                                      "volunteers_count" => event.shifts.first.volunteers_count,
+                                      "current_user_assigned" => false
+                                    }] 
+                                  })
+    end
+  end  
 end
