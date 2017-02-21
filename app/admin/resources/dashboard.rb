e0f05b9b162f27571460cb171f10d4e5d62e66c0
Benjamin Groessing <contact@BYTEQ.com>
2017-02-21 18:23:55 +0100

    Admin statistics: Add active NGO count

--- app/admin/resources/dashboard.rb
+++ app/admin/resources/dashboard.rb
@@ -1,40 +1,6 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t(".active_admin.dashboard")}, url: "/admin", html_options: { "data-turbolinks": "false" }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    columns do
-      column do
-        panel I18n.t("active_admin.statistics.helper_statistics") do
-          h4 I18n.t("active_admin.statistics.event_count")
-          h2 do
-            strong StatisticOperation::EventCount::Show.new.()
-          end
-
-          h4 I18n.t("active_admin.statistics.participants_upcoming")
-          h2 do
-            strong StatisticOperation::TotalParticipants::Upcoming.new.()
-          end
-
-          h4 I18n.t("active_admin.statistics.participants_past")
-          h2 do
-            strong StatisticOperation::TotalParticipants::Past.new.()
-          end
-
-          h4 I18n.t("active_admin.statistics.volunteers_needed")
-          h2 do
-            strong StatisticOperation::RequiredVolunteers::Show.new.()
-          end
-
-          h4 I18n.t("active_admin.statistics.work_hours")
-          h2 do
-            strong StatisticOperation::VolunteerWorkHours::Show.new.()
-          end
-        end
-      end
-    end
-  end
-
-
   page_action :date_filtered, method: :post do
     from_date = Date.parse(params[:from_date])
     to_date = Date.parse(params[:to_date])
@@ -78,7 +44,7 @@ ActiveAdmin.register_page "Dashboard" do
         attributes_table_for "" do
           row "Registrierungen" do
             div "#{User.unscoped.count} Freiwillige"
-            div "#{Ngo.unscoped.count} NGOs"
+            div "#{Ngo.unscoped.count} NGOs (davon #{Ngo.confirmed.where.not(confirmed_at: nil).count} aktiv*)"
           end
           row "Schichtbeginne" do
             div "#{Shift.unscoped.count} Schichten"
@@ -234,7 +200,8 @@ ActiveAdmin.register_page "Dashboard" do
           end
         end
         div style: "color: #b3bcc1;" do
-          "Bericht erstellt am #{l(Time.zone.now, format: :short)}"
+          div "* Email bestätigt, von Administrator bestätigt und nicht gelöscht."
+          div "Bericht erstellt am #{l(Time.zone.now, format: :short)}"
         end
 
       end
