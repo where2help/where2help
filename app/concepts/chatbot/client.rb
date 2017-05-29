12967cc58f741d258227f7002531b614441185c0
Aaron Cruz <aaron@aaroncruz.com>
2017-05-29 15:13:19 +0200

    Reset client to before bad spike

--- app/concepts/chatbot/client.rb
+++ app/concepts/chatbot/client.rb
@@ -2,9 +2,8 @@ module Chatbot
   class Client
     attr_reader :client
 
-    def initialize(send_immediately = ENV.fetch("SEND_BOT_MESSAGES_IMMEDIATELY", false))
-      @send_immediately = send_immediately.to_s == "true"
-      @client           = MessengerClient::Client.new(ENV.fetch("FB_MESSENGER_PAGE_TOKEN"))
+    def initialize
+      @client = MessengerClient::Client.new(ENV.fetch("FB_MESSENGER_PAGE_TOKEN"))
     end
 
     def set_get_started
@@ -23,33 +22,20 @@ module Chatbot
       text(fb_id, msg)
     end
 
-    def text(fb_id, msg)
-      send_later(fb_id, text: msg)
-    end
-
     def send_button_template(user, text, buttons)
       fb_id = user.facebook_account&.facebook_id
       return if fb_id.nil?
       tpl  = MessengerClient::ButtonTemplate.new(text, buttons)
       data = tpl.to_json
-      send_later(fb_id, data)
+      res  = client.send(fb_id, data)
+      log_res(res)
+      record_message(fb_id, data)
     end
 
-    def send_later(recipient_id, data)
-      acct = FacebookAccount.find_by(facebook_id: recipient_id)
-      if acct.present?
-        bot_message = acct.bot_messages.create(
-          from_bot:   false,
-          payload:    data
-        )
-        send_message(bot_message) if send_immediately?
-      else
-        Rails.logger.warn("Chatbot::Client#send_later - Couldn't find account for FB ID #{recipient_id.inspect}")
-      end
-    end
-
-    def send_immediately?
-      @send_immediately
+    def text(fb_id, msg)
+      res = client.text(recipient_id: fb_id, text: msg) unless fb_id.nil?
+      log_res(res)
+      record_message(fb_id, msg)
     end
 
     def get_profile(fb_id, scopes = %w(locale first_name last_name timezone))
@@ -58,22 +44,14 @@ module Chatbot
       FacebookUser.new(res)
     end
 
-    def send_message(chatbot_message, now = Time.now)
-      payload      = chatbot_message.payload
-      recipient_id = chatbot_message.account.facebook_id
-      res = @client.send(recipient_id, payload)
-      repo = Chatbot::Repo.new
-      if res.success?
-        repo.log_sent(chatbot_message, time)
-      else
-        repo.new.log_message_error(chatbot_message, res.body, now)
-      end
-      log_res(res, chatbot_message_id: chatbot_message.id)
-      res
+    def record_message(fb_id, msg)
+      acct = FacebookAccount.find_by(facebook_id: fb_id)
+      return if acct.nil?
+      acct.bot_messages.create(provider: :facebook, from_bot: true, payload: msg)
     end
 
-    def log_res(res, opts = {})
-      Rails.logger.info("FB MESSENGER RESPONSE #{res.success? ? '' : 'UN'}SUCCESSFUL (#{res.code})\nBODY: #{res.body}\nOPTS: #{opts.inspect}")
+    def log_res(res)
+      Rails.logger.info("FB MESSENGER RESPONSE #{res.success? ? '' : 'UN'}SUCCESSFUL (#{res.code})\nBODY: #{res.body}")
     end
   end
 end
