b3e46729d5af8ef90b3828a8b4653b0a663717a6
Martin Keiblinger <keiblinger@gmx.at>
2017-06-02 11:36:44 +0200

    Added redirect to login for user who have been logged in either as NGO or regular volunteer.
    Additionally, I fixed the issue of applying style for new, not-logged in user to all root pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  include ActionView::Helpers::UrlHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_locale
+  before_action :set_locale, :redirect_to_login_if_possible
 
   def set_locale
     locale_param = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : nil
@@ -15,5 +16,20 @@ class ApplicationController < ActionController::Base
 
   def self.default_url_options(options = {})
     { locale: I18n.locale }.merge options
+  end
+
+  def redirect_to_login_if_possible
+    valid_roles = ['user', 'ngo']
+
+    is_role_known = valid_roles.include?(cookies[:last_role])
+    is_root_page = current_page?('/')
+
+    if(request.get? && is_role_known && is_root_page)
+      if(cookies[:last_role] == 'user')
+        redirect_to new_user_session_path
+      else
+        redirect_to new_ngo_session_path
+      end
+    end
   end
 end
--- app/controllers/ngos/sessions_controller.rb
+++ app/controllers/ngos/sessions_controller.rb
@@ -12,9 +12,10 @@ class Ngos::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    cookies[:last_role] = 'ngo'
+    super
+  end
 
   # protected
 
