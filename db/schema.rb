22d275b552a334312620389fe86c8324be13273b
Benjamin Groessing <benjamin@byteq.com>
2017-06-08 14:05:54 +0200

    Update chatbot translations

--- db/schema.rb
+++ db/schema.rb
@@ -248,6 +248,7 @@ ActiveRecord::Schema.define(version: 20170529131742) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", where: "(deleted_at IS NULL)", using: :btree
     t.index ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
     t.index ["email"], name: "index_users_on_email", where: "(deleted_at IS NULL)", using: :btree
+    t.index ["locked_at"], name: "index_users_on_locked_at", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", where: "(deleted_at IS NULL)", using: :btree
   end
 
