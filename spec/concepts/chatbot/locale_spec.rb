9e3595030d5cce3c91e230d418db53beba8048ad
Aaron Cruz <aaron@aaroncruz.com>
2017-07-06 13:05:41 +0200

    chatbot locale spec

---
+++ spec/concepts/chatbot/locale_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe Chatbot::Locale, focus: true do
+  context ".from_facebook" do
+    it "converts to rails style locale" do
+      expect(Chatbot::Locale.from_facebook("en_US").locale).to eq("en")
+    end
+
+    it "returns default (en) on unknown" do
+      expect(Chatbot::Locale.from_facebook("foo_BAR").locale).to eq("en")
+    end
+  end
+
+  context ".from_w2h" do
+    it "converts to facebook locale" do
+      expect(Chatbot::Locale.from_w2h("en").facebook_locale).to eq("en_US")
+    end
+  end
+
+  context ".default" do
+    it "defaults to english" do
+      expect(Chatbot::Locale.default.locale).to eq("en")
+    end
+  end
+end
