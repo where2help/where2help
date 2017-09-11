05f73e2648fa8904305667ab7837c5fd944f74f9
Aaron Cruz <pferdefleisch+git@gmail.com>
2017-09-11 14:36:47 +0200

    i hate this

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
     domain:               'gmail.com',
     user_name:            ENV["SMTP_EMAIL_ADDRESS"],
     password:             ENV["SMTP_EMAIL_PASSWORD"],
-    authentication:       :plain,
+    authentication:       :login,
     enable_starttls_auto: true
   }
 end
