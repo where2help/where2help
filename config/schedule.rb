962cdaab8864afe3af5d22aa9d2742cf6a0958ed
Benjamin Groessing <benjamin@byteq.com>
2017-09-19 12:15:24 +0200

    Add notifications tasks to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,12 @@ set :environment, ENV['RAILS_ENV']
 every 1.month, at: '4:00 am' do
   rake 'db:anonymize_deleted_users'
 end
+
+every 1.day, at: '5:00 pm' do
+  rake 'user:notify'
+end
+
+every 1.day, at: '6:00 pm' do
+  rake 'user:send_unsent_notifications'
+end
+
